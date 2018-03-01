303b23a2daa6fadd1d714803300873caeae6ebf0
tkobernyk <taras.kobernyk@gmail.com>
2018-03-02 01:36:29 +0200

    Captch

---
+++ Captcha/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using CaptchaMvc.Attributes;
+using CaptchaMvc.HtmlHelpers;
+
+namespace Captcha.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [CaptchaVerify("Captcha is not valid")]
+        public ActionResult Index(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ErrMessage = "Error: captcha is not valid.";
+            }
+            return View();
+        }
+    }
+}
